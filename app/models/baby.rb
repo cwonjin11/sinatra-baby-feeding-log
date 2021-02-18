3ece7cbff2e8ed4e0e5e82df18db6d5e1be910c1
Wonjin <cwonjin1@gmail.com>
2021-02-17 23:43:59 -0500

    class Baby 'validates' modified.

--- app/models/baby.rb
+++ app/models/baby.rb
@@ -1,6 +1,6 @@
 class Baby < ActiveRecord::Base
     has_many :schedules
     has_many :users, through: :schedules
-    validates :name, :age, :user_id, presence: true
-  
+    validates :name, :age,  presence: true
+  #In order for the user to be redirected after signing up, their input needs to be validated so that bad data does not get saved into the data base
 end
