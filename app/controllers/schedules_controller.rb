ce23dc5530b9890c5287a68f47a51208df1d9d89
Wonjin <cwonjin1@gmail.com>
2021-02-22 11:37:08 -0500

    if loggin? function modified. User can only create/edit/delete their own. So <baby/:id> & <schedule/:id> 'if statement' updated.

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -2,7 +2,7 @@ class SchedulesController < ApplicationController
 
     get '/schedules/:id' do
         @scheme = Schedule.find(params[:id])
-        if logged_in? 
+        if logged_in? && @baby.user_id == current_user.id
             @babies = Baby.all
             @schedules = Schedule.all
             @scheme = Schedule.find(params[:id])
@@ -18,15 +18,18 @@ class SchedulesController < ApplicationController
 
     get '/babies/:id/schedules/new' do
         @baby = Baby.find(params[:id])
-        if logged_in? 
-        erb :'schedules/new'
+        if logged_in? && @baby.user_id == current_user.id
+            erb :'schedules/new'
+        else
+            redirect to("/babies/#{@baby.id}")
         end
     end
 
 
     post '/babies/:id/schedules' do
-        if logged_in?
-            @baby = Baby.find(params[:id])
+        @baby = Baby.find(params[:id])
+        if logged_in? && @baby.user_id == current_user.id
+            
             @scheme = Schedule.new(feeding_type: params["feeding_type"], start_time: params["start_time"], 
                     end_time: params["end_time"], total_amount: params["total_amount"], baby_id: @baby.id, user_id: current_user.id)
             if !@scheme.save
@@ -35,9 +38,9 @@ class SchedulesController < ApplicationController
             else 
                 redirect to to("/babies/#{@baby.id}")
             end
-            # redirect to("/babies/#{@baby.id}") 
+             
         else 
-            redirect to('/login')
+            redirect to("/babies/#{@baby.id}")
         end
     end
     
@@ -59,23 +62,26 @@ class SchedulesController < ApplicationController
      
 
     get '/babies/:baby_id/schedules/:id/update' do  
-        #  binding.pry
-        if logged_in? 
+       
+        @baby = Baby.find(params[:baby_id])
+        @scheme = Schedule.find(params[:id])
+        # binding.pry
+        if logged_in? && @baby.user_id == current_user.id && current_user.id == @scheme.user_id
             @user = User.find(session[:user_id])
             @scheme = Schedule.find(params[:id])
             @baby = Baby.find(params[:baby_id])
             # @scheme.feeding_type = params[:feeding_type]
             @scheme.save
-        erb :'/schedules/update'
+            erb :'/schedules/update'
         else
-        redirect to('/login')
+            redirect to("/babies/#{@baby.id}")
         end
     end
 
 
     delete '/babies/:baby_id/schedules/:id/delete' do
         @scheme = Schedule.find(params[:id])
-        if logged_in?
+        if logged_in? 
             @scheme.destroy
             @baby = Baby.find(@scheme.baby_id)
             redirect to("/babies/#{@baby.id}")
