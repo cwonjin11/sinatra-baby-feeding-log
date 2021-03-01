3919e4c074d7804e4e13a4703eea8fdd8f76f5c6
Wonjin <cwonjin1@gmail.com>
2021-03-01 12:04:24 -0500

    delete uncommented line

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,8 @@ class User < ActiveRecord::Base
     
     validates_presence_of :username, :email, :password
     validates_uniqueness_of :username, :email
-    #In order for the user to be redirected after signing up, their input needs to be validated so that bad data does not get saved into the data base
+  
     has_secure_password 
-    #Password Encryption
-    #Password encryption was required to protect user accounts from getting hacked. It uses a hashing algorithm to scramble and salt passwords. In order to implement this we installed a gem called bcrypt and my User’s table had a password_digest column. I then added has_secure_password to my User model and now have all the functionalities of the bcrypt gem.
+    
 
 end
